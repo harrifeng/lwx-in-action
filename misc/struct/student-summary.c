8b42fb5816dd1afc4aafeece4915006bc433def4
Harri Feng <harri.feng@gmail.com>
2014-12-14 18:38:54 -0800

    remove unnecessary

--- misc/struct/student-summary.c
+++ misc/struct/student-summary.c
@@ -12,12 +12,7 @@ struct Stu {
 
 int main(int argc, char *argv[])
 {
-    FILE *fp;
-    char *line = NULL;
-    size_t len;
-    ssize_t read;
     struct Stu stuarr[100];
-    int index = 0;
 
     int total = 0;
     int i;
@@ -27,6 +22,9 @@ int main(int argc, char *argv[])
         scanf("%s %s %d %d %d", stuarr[i].name, stuarr[i].number,
               &stuarr[i].sub1, &stuarr[i].sub2, &stuarr[i].sub3);
     }
+    /* 你就应该在这里进行排序啦! 排序用到一个叫做qsort的函数,你得学习下了... */
+    /* qsort这个函数在lwx-in-action这个工程里面有,你要用"grep"这个bash */
+    /* 命令在工程里面找到它... */
 
     for (i = 0; i < total; i++) {
         printf("%s %s %d %d %d\n", stuarr[i].name, stuarr[i].number,
