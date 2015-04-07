7fbdc2aeb319ca3108ab1b1756372e136435b306
harri feng <harrifeng@gmail.com>
2015-04-07 20:48:49 +0800

    finish the language parts

--- cpp/execise/question5.cc
+++ cpp/execise/question5.cc
@@ -2,19 +2,26 @@
 using namespace std;
 
 class Point {
+// data part should be private, use public to be able to output
 public:
     double x;
     double y;
 public:
+    // default constructor
     Point():x(0.0), y(2.0){};
+    // constructor with parameter
     Point(double dx, double dy):x(dx), y(dy){};
+    // copy constructor
     Point(const Point& other) {
-        x = other.x;
-        y = other.y;
+        if (&other != this) {
+            x = other.x;
+            y = other.y;
+        }
     }
 };
 
 class Line{
+// data part should be private, use public to be able to output
 public:
     Point a;
     Point b;
@@ -22,11 +29,14 @@ public:
     Line():a(Point()), b(Point()){};
     Line(Point pa, Point pb): a(pa), b(pb){}
     Line(const Line& other) {
-        a = Point(other.a);
-        b = Point(other.b);
+        if (&other != this) {
+            a = Point(other.a);
+            b = Point(other.b);
+        }
     }
 };
 class Triangle {
+// data part should be private, use public to be able to output
 public:
     Line l1;
     Line l2;
@@ -34,9 +44,11 @@ public:
 public:
     Triangle(Line la, Line lb, Line lc): l1(la), l2(lb), l3(lc) {}
     Triangle(const Triangle& other) {
-        l1 = Line(other.l1);
-        l2 = Line(other.l2);
-        l3 = Line(other.l3);
+        if (&other != this) {
+            l1 = Line(other.l1);
+            l2 = Line(other.l2);
+            l3 = Line(other.l3);
+        }
     }
     bool isTriangle();
     double triangleSize();
@@ -76,3 +88,10 @@ int main(int argc, char *argv[])
     cout << t2.l1.a.x << endl;
     return 0;
 }
+
+////////////////////////////////////////////////////
+// <===================OUTPUT===================> //
+// c:\code\lwx-in-action\cpp\execise>a.exe        //
+// a.exe                                          //
+// 30                                             //
+////////////////////////////////////////////////////
